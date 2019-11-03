34d9c733a523dcb3c9c780d76105fd8911893277
Dennis Yurichev <dennis@yurichev.com>
2019-11-03 12:00:52 +0200

    an example

---
+++ advanced/450_more_ptrs/62.c
@@ -0,0 +1,18 @@
+int expired()
+{
+	// check license key, current date/time, etc
+};
+
+int main()
+{
+	if (expired) // must be expired() here
+	{
+		print ("expired\n");
+		exit(0);
+	}
+	else
+	{
+		// do something
+	};
+};
+
