49eafa0cbae7a49f4d4d577e1874de1885bd8b45
Dennis Yurichev <dennis@yurichev.com>
2019-11-28 12:02:18 +0200

    ...

--- patterns/00_empty/dbg_print_IT.c
+++ patterns/00_empty/dbg_print_IT.c
@@ -1,17 +1,17 @@
 void dbg_print (const char *fmt, ...)
 {
 #ifdef _DEBUG
-	// apri file di log
-	// scrivi nel file di log
-	// chiudi il file di log
+        // apri file di log
+        // scrivi nel file di log
+        // chiudi il file di log
 #endif
 };
 
 void some_function()
 {
-	...
+        ...
 
-	dbg_print ("we did something\n");
+        dbg_print ("we did something\n");
 
-	...
+        ...
 };
--- patterns/00_empty/dbg_print_RU.c
+++ patterns/00_empty/dbg_print_RU.c
@@ -1,17 +1,17 @@
 void dbg_print (const char *fmt, ...)
 {
 #ifdef _DEBUG
-	// открыть лог-файл
-	// записать в лог-файл
-	// закрыть лог-файл
+        // открыть лог-файл
+        // записать в лог-файл
+        // закрыть лог-файл
 #endif
 };
 
 void some_function()
 {
-	...
+        ...
 
-	dbg_print ("we did something\n");
+        dbg_print ("we did something\n");
 
-	...
+        ...
 };
