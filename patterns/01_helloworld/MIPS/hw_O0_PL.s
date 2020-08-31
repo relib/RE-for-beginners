f708dd143792e9ec008683872a8a8f8e3811aae3
Marcin Sokołowski <sokolekster@gmail.com>
2020-08-31 10:40:53 +0200

    [PL] finished correction of 01_helloworld and unify some other parts of the text

--- patterns/01_helloworld/MIPS/hw_O0_PL.s
+++ patterns/01_helloworld/MIPS/hw_O0_PL.s
@@ -1,39 +1,39 @@
 $LC0:
 	.ascii	"Hello, world!\012\000"
 main:
-; prolog f-cji
-; odłożyć RA (\$31) i FP na stos:
+; prolog funkcji
+; odłóż RA (\$31) i FP na stos:
 	addiu	$sp,$sp,-32
 	sw	$31,28($sp)
 	sw	$fp,24($sp)
-; ustawić FP (stack frame pointer):
+; ustaw FP (stack frame pointer):
 	move	$fp,$sp
-; ustawić GP:
+; ustaw GP:
 	lui	$28,%hi(__gnu_local_gp)
 	addiu	$28,$28,%lo(__gnu_local_gp)
-; załadować adres linii tekstowej:
+; załaduj adres łańcucha znaków
 	lui	$2,%hi($LC0)
 	addiu	$4,$2,%lo($LC0)
-; załadować adres f-cji puts() korzystając z GP:
+; załaduj adres funkcji puts() z GP:
 	lw	$2,%call16(puts)($28)
 	nop
-; wywołać puts():
+; wywołaj puts():
 	move	$25,$2
 	jalr	$25
 	nop  ; branch delay slot
 
-; przywrócić GP ze stosu lokalnego:
+; przywróć GP ze stosu lokalnego:
 	lw	$28,16($fp)
-; ustawić rejestr \$2 (\$V0) na zero:
+; ustaw rejestr \$2 (\$V0) na zero:
 	move	$2,$0
 ; epilog funkcji.
-; przywrócić SP:
+; przywróć SP:
 	move	$sp,$fp
-; przywrócić RA:
+; przywróć RA:
 	lw	$31,28($sp)
-; przywrócić FP:
+; przywróć FP:
 	lw	$fp,24($sp)
 	addiu	$sp,$sp,32
-; przejście do RA:
+; skok pod adres z RA:
 	j	$31
 	nop  ; branch delay slot
